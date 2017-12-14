e01180360fa76b26476e659da7a07e5623bc0fc9
95roma <romarioavr@gmail.com>
2017-12-13 23:00:46 -0600

    muestra mision y vision y logo de la casa

--- plantilla/barraSuperior.php
+++ plantilla/barraSuperior.php
@@ -4,7 +4,7 @@
                 <div class="navbar-header">
                     <a href="#" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse" aria-expanded="false"></a>
                     <a href="#" class="bars"></a>
-                    <a class="navbar-brand" href="index.html">Casa de Encuentro Juevenil</a>
+                    <a class="navbar-brand" href="../index.php">Casa de Encuentro Juevenil</a>
                 </div>
                 <div class="collapse navbar-collapse" id="navbar-collapse">
                     <ul class="nav navbar-nav navbar-right">
